be01ab1e2eb523a0778c1d6b9825fbfe5a5c9c9d
Jonathan <detective.redd@gmail.com>
2023-09-15 20:40:56 -0400

    added naive servant

--- Avalon/baseline_agents.py
+++ Avalon/baseline_agents.py
@@ -1,5 +1,6 @@
 import random
 from engine import AvalonConfig
+import itertools
 
 class Agent:
 
@@ -143,15 +144,25 @@ class NaiveMerlin(Agent):
 
 class NaiveServant(Agent):
 
-    def __init__(self, id, name, config: AvalonConfig, side=1, role=5, sides = None):
+    def __init__(self, id, name, config: AvalonConfig, side=1, role=5, sides = None, lexigraphic = True):
         super().__init__(id, name, config, side, role, sides)
 
         # maintain a list of all possible combinations of player sides
         self.possible_player_sides = self.generate_possible_player_sides(self.sides)
-        # maintain preferences for each possible team given team size
-        self.team_preferences = self.generate_team_preferences()
+        self.player_side_probabilities = [1/len(self.possible_player_sides)] * len(self.possible_player_sides)
+
+        # generate team preferences for first mission
+        self.team_preferences = self.generate_team_preferences(0)
+
+        # record the largest and most recent team that succeeded
+        self.largest_successful_team = None
+
+        self.lexigraphic = lexigraphic
 
     def generate_possible_player_sides(self, sides):
+        '''
+        generates a list of all possible combinations of player sides given a list of known sides and unknown sides recursively
+        '''
         out = []
         # if there are no unknown sides, return the list of sides   
         if -1 not in sides:
@@ -165,6 +176,70 @@ class NaiveServant(Agent):
                 sides_copy[unknown_index] = side
                 out.extend(self.generate_possible_player_sides(sides_copy))
             return out
+        
+    def generate_team_preferences(self, mission_id):
+        '''
+        generates preferences across mission teams specified by mission_id
+        '''
+        team_size = self.config.num_players_for_quest[mission_id]
+        # generate list of all possible teams of size team_size, where team should be a list of player ids
+        teams = list(itertools.combinations(range(self.config.num_players), team_size))
+
+        # maintain a list of preferences for each team, and set all preferences to 0
+        team_preferences = [0] * len(teams)
+
+        # iterate over possible player sides and side probabilities
+        for sides, prob in zip(self.possible_player_sides, self.player_side_probabilities):
+            # iterate over possible teams
+            for team in teams:
+                # if team is all good in sides, increment team preference by prob of side
+                if all([sides[i] == 1 for i in team]):
+                    team_preferences[teams.index(team)] += prob
+        
+        # return dictionary mapping teams to preferences
+        return dict(zip(teams, team_preferences))
+    
+    def find_most_prefered_teams(self, team_to_preferences):
+        '''
+        returns a list of the most preferred teams, where team_to_preferences is a dictionary mapping teams to preferences
+        '''
+        # find the maximum preference
+        max_preference = max(team_to_preferences.values())
+        # return a list of all teams with maximum preference
+        max_teams = [team for team, preference in team_to_preferences.items() if preference == max_preference]
+        # if there is only one team with maximum preference, return it
+        if len(max_teams) == 1:
+            return max_teams[0]
+        # else return list of teams of max_teams that are subsets of self.largest_successful_team if it is not None and non-empty, otherwise return max_teams
+        else:
+            if self.largest_successful_team is not None and self.lexigraphic:
+                out = [team for team in max_teams if set(team).issubset(set(self.largest_successful_team))]
+                if len(out) > 0:
+                    return out
+            return max_teams
+    
+    def vote_on_team(self, mission_id, team):
+        # if team is in most preferred teams, approve, otherwise reject
+        return 1 if team in self.find_most_prefered_teams(self.team_preferences) else 0
+    
+    def propose_team(self, mission_id):
+        # propose random team in most preferred teams
+        return random.choice(self.find_most_prefered_teams(self.team_preferences))
+    
+    def observeMission(self, team, mission_id, num_fails):
+        # if mission succeeded, update largest_successful_team
+        if num_fails == 0:
+            if self.largest_successful_team is None or len(team) > len(self.largest_successful_team):
+                self.largest_successful_team = team
+
+        # set the probability of all sides that have at least num_fails evil players on the team to 0
+        for sides, prob in zip(self.possible_player_sides, self.player_side_probabilities):
+            if sum([sides[i] == 0 for i in team]) >= num_fails:
+                self.player_side_probabilities[self.possible_player_sides.index(sides)] = 0
+        pass
+    
+
+
         
         
     
